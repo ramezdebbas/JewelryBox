7fbcf22b24e29c43c3cc53bd81a6e892427f0b82
Rameez <debbas@outlook.com>
2013-04-11 12:19:44 +0500

    Complete App v1.0

---
+++ Jewelry Box/EcommFashion/DataModel/SampleDataSource.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Collections.Specialized;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model that supports notification when members are added, removed, or modified.  The property
+// names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs.
+
+namespace EcommFashion.Data
+{
+    /// <summary>
+    /// Base class for <see cref="SampleDataItem"/> and <see cref="SampleDataGroup"/> that
+    /// defines properties common to both.
+    /// </summary>
+    [Windows.Foundation.Metadata.WebHostHidden]
+    public abstract class SampleDataCommon : EcommFashion.Common.BindableBase
+    {
+        private static Uri _baseUri = new Uri("ms-appx:///");
+
+        public SampleDataCommon(String uniqueId, String title, String subtitle, String imagePath, String description)
+        {
+            this._uniqueId = uniqueId;
+            this._title = title;
+            this._subtitle = subtitle;
+            this._description = description;
+            this._imagePath = imagePath;
+        }
+
+        private string _uniqueId = string.Empty;
+        public string UniqueId
+        {
+            get { return this._uniqueId; }
+            set { this.SetProperty(ref this._uniqueId, value); }
+        }
+
+        private string _title = string.Empty;
+        public string Title
+        {
+            get { return this._title; }
+            set { this.SetProperty(ref this._title, value); }
+        }
+
+        private string _subtitle = string.Empty;
+        public string Subtitle
+        {
+            get { return this._subtitle; }
+            set { this.SetProperty(ref this._subtitle, value); }
+        }
+
+        private string _description = string.Empty;
+        public string Description
+        {
+            get { return this._description; }
+            set { this.SetProperty(ref this._description, value); }
+        }
+
+        private ImageSource _image = null;
+        private String _imagePath = null;
+        public ImageSource Image
+        {
+            get
+            {
+                if (this._image == null && this._imagePath != null)
+                {
+                    this._image = new BitmapImage(new Uri(SampleDataCommon._baseUri, this._imagePath));
+                }
+                return this._image;
+            }
+
+            set
+            {
+                this._imagePath = null;
+                this.SetProperty(ref this._image, value);
+            }
+        }
+
+        public void SetImage(String path)
+        {
+            this._image = null;
+            this._imagePath = path;
+            this.OnPropertyChanged("Image");
+        }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class SampleDataItem : SampleDataCommon
+    {
+        public SampleDataItem(String uniqueId, String title, String subtitle, String imagePath, String description, String content, SampleDataGroup group)
+            : base(uniqueId, title, subtitle, imagePath, description)
+        {
+            this._content = content;
+            this._group = group;
+        }
+
+        private string _content = string.Empty;
+        public string Content
+        {
+            get { return this._content; }
+            set { this.SetProperty(ref this._content, value); }
+        }
+
+        private SampleDataGroup _group;
+        public SampleDataGroup Group
+        {
+            get { return this._group; }
+            set { this.SetProperty(ref this._group, value); }
+        }
+    }
+
+    /// <summary>
+    /// Generic group data model.
+    /// </summary>
+    public class SampleDataGroup : SampleDataCommon
+    {
+        public SampleDataGroup(String uniqueId, String title, String subtitle, String imagePath, String description)
+            : base(uniqueId, title, subtitle, imagePath, description)
+        {
+            Items.CollectionChanged += ItemsCollectionChanged;
+        }
+
+        private void ItemsCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            // Provides a subset of the full items collection to bind to from a GroupedItemsPage
+            // for two reasons: GridView will not virtualize large items collections, and it
+            // improves the user experience when browsing through groups with large numbers of
+            // items.
+            //
+            // A maximum of 12 items are displayed because it results in filled grid columns
+            // whether there are 1, 2, 3, 4, or 6 rows displayed
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewStartingIndex < 12)
+                    {
+                        TopItems.Insert(e.NewStartingIndex, Items[e.NewStartingIndex]);
+                        if (TopItems.Count > 12)
+                        {
+                            TopItems.RemoveAt(12);
+                        }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    if (e.OldStartingIndex < 12 && e.NewStartingIndex < 12)
+                    {
+                        TopItems.Move(e.OldStartingIndex, e.NewStartingIndex);
+                    }
+                    else if (e.OldStartingIndex < 12)
+                    {
+                        TopItems.RemoveAt(e.OldStartingIndex);
+                        TopItems.Add(Items[11]);
+                    }
+                    else if (e.NewStartingIndex < 12)
+                    {
+                        TopItems.Insert(e.NewStartingIndex, Items[e.NewStartingIndex]);
+                        TopItems.RemoveAt(12);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldStartingIndex < 12)
+                    {
+                        TopItems.RemoveAt(e.OldStartingIndex);
+                        if (Items.Count >= 12)
+                        {
+                            TopItems.Add(Items[11]);
+                        }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    if (e.OldStartingIndex < 12)
+                    {
+                        TopItems[e.OldStartingIndex] = Items[e.OldStartingIndex];
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    TopItems.Clear();
+                    while (TopItems.Count < Items.Count && TopItems.Count < 12)
+                    {
+                        TopItems.Add(Items[TopItems.Count]);
+                    }
+                    break;
+            }
+        }
+
+        private ObservableCollection<SampleDataItem> _items = new ObservableCollection<SampleDataItem>();
+        public ObservableCollection<SampleDataItem> Items
+        {
+            get { return this._items; }
+        }
+
+        private ObservableCollection<SampleDataItem> _topItem = new ObservableCollection<SampleDataItem>();
+        public ObservableCollection<SampleDataItem> TopItems
+        {
+            get { return this._topItem; }
+        }
+    }
+
+    /// <summary>
+    /// Creates a collection of groups and items with hard-coded content.
+    /// 
+    /// SampleDataSource initializes with placeholder data rather than live production
+    /// data so that sample data is provided at both design-time and run-time.
+    /// </summary>
+    public sealed class SampleDataSource
+    {
+        private static SampleDataSource _sampleDataSource = new SampleDataSource();
+
+        private ObservableCollection<SampleDataGroup> _allGroups = new ObservableCollection<SampleDataGroup>();
+        public ObservableCollection<SampleDataGroup> AllGroups
+        {
+            get { return this._allGroups; }
+        }
+
+        public static IEnumerable<SampleDataGroup> GetGroups(string uniqueId)
+        {
+            if (!uniqueId.Equals("AllGroups")) throw new ArgumentException("Only 'AllGroups' is supported as a collection of groups");
+
+            return _sampleDataSource.AllGroups;
+        }
+
+        public static SampleDataGroup GetGroup(string uniqueId)
+        {
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.AllGroups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static SampleDataItem GetItem(string uniqueId)
+        {
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.AllGroups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public SampleDataSource()
+        {
+            String ITEM_CONTENT = String.Format("Item Content: {0}\n\n{0}\n\n{0}\n\n{0}\n\n{0}\n\n{0}\n\n{0}",
+                        "Nivax Data");
+
+            var group1 = new SampleDataGroup("Group-1",
+                    "Necklaces",
+                    "Group Subtitle: 1",
+                    "Assets/DarkGray.png",
+                    "Group Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor scelerisque lorem in vehicula. Aliquam tincidunt, lacus ut sagittis tristique, turpis massa volutpat augue, eu rutrum ligula ante a ante");
+            group1.Items.Add(new SampleDataItem("Group-1-Item-1",
+                    "Fringe Crescent Necklace",
+                    "",
+                    "Assets/HubPage/HubPage1.png",
+                    "A necklace featuring an etched crescent pendant with faux turquoise accents, etched feathers, and chain fringe pendants. High polish and matte finishes. Lobster clasp. Medium weight",
+                    ITEM_CONTENT,
+                    group1));
+            group1.Items.Add(new SampleDataItem("Group-1-Item-2",
+                   "Painted Cutout Bib Necklace",
+                    "",
+                    "Assets/HubPage/HubPage2.png",
+                    "A high polish chain necklace featuring a linked cutout bib with a painted finish. Lobster clasp. Medium weight.",
+                    ITEM_CONTENT,
+                    group1));
+            group1.Items.Add(new SampleDataItem("Group-1-Item-3",
+                    "Tribal-Style Bib Necklace",
+                    "",
+                    "Assets/HubPage/HubPage3.png",
+                    "A rolo chain featuring an etched bib with a tribal-style design. High polish finish. Lobster clasp. Medium weight.",
+                    ITEM_CONTENT,
+                    group1));
+            group1.Items.Add(new SampleDataItem("Group-1-Item-4",
+                    "Crescent Charm Necklace",
+                    "",
+                    "Assets/HubPage/HubPage4.png",
+                    "A snake chain necklace featuring gradated lacquered crescent charms. Pressed trim. Lobster clasp. High polish finish. Lightweight.",
+                    ITEM_CONTENT,
+                    group1));
+            group1.Items.Add(new SampleDataItem("Group-1-Item-5",
+                    "Leaf Bib Necklace",
+                    "",
+                    "Assets/HubPage/HubPage5.png",
+                    "A short rolo chain necklace featuring a lacquered leaf bib. Lobster clasp. High polish finish. Lightweight.",
+                    ITEM_CONTENT,
+                    group1));
+            group1.Items.Add(new SampleDataItem("Group-1-Item-6",
+                    "Twisted Rhinestone Necklace",
+                    "",
+                    "Assets/HubPage/HubPage6.png",
+                    "A multi-strand necklace featuring large faceted beads and twisted rhinestones. Lobster clasp. High polish finish. Lightweight.",
+                    ITEM_CONTENT,
+                    group1));
+            this.AllGroups.Add(group1);
+
+             var group2 = new SampleDataGroup("Group-2",
+                    "Rings",
+                    "Group Subtitle: 2",
+                    "Assets/LightGray.png",
+                    "Group Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor scelerisque lorem in vehicula. Aliquam tincidunt, lacus ut sagittis tristique, turpis massa volutpat augue, eu rutrum ligula ante a ante");
+            group2.Items.Add(new SampleDataItem("Group-2-Item-1",
+					"Cutout Arrow Ring",
+					"",
+                    "Assets/HubPage/HubPage7.png",
+                    "A burnished ring featuring cutout details and etched arrows. Lightweight.",
+                    ITEM_CONTENT,
+                    group2));
+            group2.Items.Add(new SampleDataItem("Group-2-Item-2",
+                    "Southwestern Ring Set",
+                    "",
+                    "Assets/HubPage/HubPage8.png",
+                    "A set of five rings featuring etched Southwestern designs. High polish finish. Lightweight.",
+                    ITEM_CONTENT,
+                    group2));
+            group2.Items.Add(new SampleDataItem("Group-2-Item-3",
+                     "Mirrored Spiked Ring",
+                    "",
+                    "Assets/HubPage/HubPage9.png",
+                    "A metal ring featuring mirrored spiked accents. Pressed trim. High polish finish. Lightweight.",
+                    ITEM_CONTENT,
+                    group2));
+            group2.Items.Add(new SampleDataItem("Group-2-Item-4",
+                    "Braided Rope Ring",
+                   "",
+                   "Assets/HubPage/HubPage10.png",
+                   "A braided ring featuring an etched twisted rope motif and lacquered trim. High polish finish. Lightweight.",
+                   ITEM_CONTENT,
+                   group2));
+            group2.Items.Add(new SampleDataItem("Group-2-Item-5",
+                    "Etched Leaf Ring",
+                   "",
+                   "Assets/HubPage/HubPage11.png",
+                   "A metal ring featuring an etched leaf. High polish finish. Lightweight.",
+                   ITEM_CONTENT,
+                   group2));
+            group2.Items.Add(new SampleDataItem("Group-2-Item-6",
+                    "Mustache Ring Set",
+                   "",
+                   "Assets/HubPage/HubPage12.png",
+                   "A set of three rings featuring lacquered mustache accents. High polish finish. Lightweight.",
+                   ITEM_CONTENT,
+                   group2));
+            this.AllGroups.Add(group2); 
+
+			
+			var group3 = new SampleDataGroup("Group-3",
+                    "Bracelets",
+                    "Group Subtitle: 3",
+                    "Assets/LightGray.png",
+                    "Group Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor scelerisque lorem in vehicula. Aliquam tincidunt, lacus ut sagittis tristique, turpis massa volutpat augue, eu rutrum ligula ante a ante");
+            group3.Items.Add(new SampleDataItem("Group-3-Item-1",
+					"Curved Arrow Bracelet",
+					"",
+                    "Assets/HubPage/HubPage13.png",
+                    "A burnished chain bracelet featuring a curved arrow. Lobster clasp. Lightweight.",
+                    ITEM_CONTENT,
+                    group3));
+            group3.Items.Add(new SampleDataItem("Group-3-Item-2",
+                    "Cross Cuff Set",
+                    "",
+                    "Assets/HubPage/HubPage14.png",
+                    "A set of two cuffs featuring crosses. Adjustable closure. High polish finish. Lightweight.",
+                    ITEM_CONTENT,
+                    group3));
+            group3.Items.Add(new SampleDataItem("Group-3-Item-3",
+                     "Ilusion Bangle",
+                    "",
+                    "Assets/HubPage/HubPage15.png",
+                    "An illusion bangle featuring two welded bands. High polish finish. Medium weight.",
+                    ITEM_CONTENT,
+                    group3));
+            group3.Items.Add(new SampleDataItem("Group-3-Item-4",
+                    "Faceted Stretchy Bracelet",
+                   "",
+                   "Assets/HubPage/HubPage16.png",
+                   "A stretchy bracelet featuring faceted pieces with lacquered detail. High polish trimmings. Lightweight. ",
+                   ITEM_CONTENT,
+                   group3));
+            group3.Items.Add(new SampleDataItem("Group-3-Item-5",
+                    "Pyramid Studded Faux Leather Bracelet",
+                   "",
+                   "Assets/HubPage/HubPage17.png",
+                   "A faux leather bracelet featuring pyramid stud accents. Adjustable snap button closure. High polish finish. Lightweight.",
+                   ITEM_CONTENT,
+                   group3));
+            group3.Items.Add(new SampleDataItem("Group-3-Item-6",
+                    "Spiked Braided Rope Bracelet",
+                   "",
+                   "Assets/HubPage/HubPage18.png",
+                   "A braided rope bracelet featuring spiked trim. Toggle closure. High polish finish. Lightweight.",
+                   ITEM_CONTENT,
+                   group3));
+            this.AllGroups.Add(group3); 
+
+
+        }
+    }
+}
